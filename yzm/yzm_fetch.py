7fae0f8ceff37145bfafac4bd4f7ec2f15f435b1
willsonluo <willsonluo@163.com>
2018-10-10 15:31:48 +0800

    Update program.

--- yzm/yzm_fetch.py
+++ yzm/yzm_fetch.py
@@ -1,6 +1,9 @@
 # -*- coding:utf-8 -*-
-import requests,time
-# 文件下载，主要下载训练集
+import requests
+import time
+import os
+import cv2
+from PIL import Image
 
 
 def download_pics(pic_name):
@@ -14,7 +17,29 @@ def download_pics(pic_name):
         f.close()
 
 
+def clean(image_clean):
+    img = cv2.imread(image_clean, 0)  # 直接读为灰度图像
+    ret, thresh1 = cv2.threshold(img, 30, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(image_clean, thresh1)
+
+
+def split():
+    img_list = os.listdir('yzm_data')
+    print(img_list)
+    for abc in range(len(img_list)):
+        clean('yzm_data' + os.sep + img_list[abc])
+        img = Image.open('yzm_data'+ os.sep + img_list[abc])
+        im = img.convert("L")
+        split_lines = [4,16,32,46,60]
+        y_min = 1
+        y_max = 23
+        for x_min, x_max in zip(split_lines[:-1], split_lines[1:]):
+            im.crop([x_min, y_min, x_max, y_max]).save('yzm_test' + os.sep + '000_%s.jpeg' % (int(time.time() * 1000000)),
+                                                       'jpeg')  # (str(c)+'.jpeg')
+
+
 if __name__ == '__main__':
-    for i in range(100):
+    for i in range(10):
         pic_name = int(time.time() * 1000000)
         download_pics(pic_name)
+    split()
--- yzm/yzm_split.py
+++ yzm/yzm_split.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
-from PIL import Image, ImageEnhance
-from PIL import *
+from PIL import Image,ImageFilter
+import cv2
 import time
 import os
-# 图片切割
+
+
+def clean(image_clean):
+    img = cv2.imread(image_clean, 0)  # 直接读为灰度图像
+    ret, thresh1 = cv2.threshold(img, 30, 255, cv2.THRESH_BINARY)
+    cv2.imwrite(image_clean, thresh1)
 
 
 def segment(im):
@@ -18,12 +23,10 @@ def segment(im):
     return im_new
 
 
-# 图片预处理，二值化，图片增强
+# 图片预处理，二值化
 def imgTransfer(f_name):
     im = Image.open(f_name)
     im = im.filter(ImageFilter.MedianFilter())
-    # enhancer = ImageEnhance.Contrast(im)
-    # im = enhancer.enhancer(1)
     im = im.convert('L')
     return im
 
@@ -39,5 +42,6 @@ if __name__ == '__main__':
     files_name = [abc for abc in os.listdir('yzm_data')]
     print(files_name)
     for i in range(len(files_name)):
+        clean('yzm_data' + os.sep + files_name[i])
         # print('yzm_data' + os.sep + files_name[i])
         cutPictures('yzm_data' + os.sep + files_name[i])
